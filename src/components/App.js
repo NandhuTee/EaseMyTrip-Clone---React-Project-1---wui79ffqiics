256881d3519fb36916e519cb42b8d6d02f2136f9
Nandhini_T <rtnandhutee@gmail.com>
2024-05-02 16:35:53 +0530

    first_commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import "../styles/App.css";
+import React, { useState,useContext } from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
-function App() {
-  return <div className="App">App</div>;
-}
+const App = () => {
+  
+  return (
+    <div>   
+      <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+      </BrowserRouter>   
+    </div>
+  );
+};
 
 export default App;
