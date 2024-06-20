650504d3cb1445052448f5f1af6108a4ec72ab31
Nandhini_T <rtnandhutee@gmail.com>
2024-06-20 09:40:10 +0530

    update

--- src/components/NavBar/ModalLogin.js
+++ src/components/NavBar/ModalLogin.js
@@ -1,28 +1,39 @@
+// Importing necessary libraries and components
 import React,{useState} from "react";
-import { useAuth } from "../Context";
-import Modal from "@mui/material/Modal";
-import Classes from "./Navbar.module.css"
-import ModalSignUp from "./ModalSignUp";
+import { useAuth } from "../Context";// Custom hook for authentication context
+import Modal from "@mui/material/Modal";// Material-UI Modal component
+import Classes from "./Navbar.module.css"// CSS module for styling
+import ModalSignUp from "./ModalSignUp";// Modal for the Sign-Up component
+
 function ModalLogin() {
-  const handleCloseLogin = () => setOpenLogin(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const { openLogin, setOpenLogin,openSignUp, setOpenSignUp, setIsLoggedIn } = useAuth();
+    // State to manage login form inputs and error messages
+    const [errorMessage, setErrorMessage] = useState("");
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [correctCredential,setCorrectCredential]=useState(false);
+ 
+  // Destructuring authentication state and methods from the custom auth context
+  const { openLogin, setOpenLogin,openSignUp, setOpenSignUp, setIsLoggedIn } = useAuth();
+
+    // Function to close the login modal
+  const handleCloseLogin = () => setOpenLogin(false);
+  
+  // Handler for email input change
   function mailInput(e) {
     const mailSet = e.target.value;
     setMail(mailSet);
   }
 
+  // Handler for password input change
   function passwordInput(e) {
     const passwordSet = e.target.value;
     setPassword(passwordSet);
   }
 
+  // Function to handle login button click
   async function handleLoginClick() {
-    
-    try {
+        try {
+          // Making a POST request to the login API
       const response = await fetch(
         "https://academics.newtonschool.co/api/v1/bookingportals/login",
         {
@@ -34,19 +45,21 @@ function ModalLogin() {
           body: JSON.stringify({
             email: mail,
             password: password,
-            appType: "facebook",
+            appType: "facebook", //// Example app type
           }),
         }
       );
       if (response.ok) {
+        // Handling successful login
         const data = await response.json();
 
-        localStorage.setItem("token", data.token);
-        localStorage.setItem("userId", data.data._id);
-        localStorage.setItem("userName",data.data.name);
+        localStorage.setItem("token", data.token);// Storing token in local storage
+        localStorage.setItem("userId", data.data._id);// Storing user ID in local storage
+        localStorage.setItem("userName",data.data.name);// Storing user name in local storage
         localStorage.setItem("photo","https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/590.jpg");
-        setIsLoggedIn(true);
-        handleCloseLogin();
+        setIsLoggedIn(true);// Updating logged-in state
+        handleCloseLogin();// Closing login modal
+        // Handling login error
       } else {
         const errorData = await response.json();
         setErrorMessage(errorData.message);
@@ -58,13 +71,14 @@ function ModalLogin() {
     }
   }
 
+  // Function to open the Sign-Up modal
   const handleOpenSignUp= () => {
     setOpenSignUp(true);
 }
   return (
     <div>
       <Modal
-        open={openLogin}
+        open={openLogin}// Modal open state
        
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
--- src/components/NavBar/ModalSignUp.js
+++ src/components/NavBar/ModalSignUp.js
@@ -1,105 +1,145 @@
-import React,{useState} from "react";
-import Classes from "./Navbar.module.css";
-import { useAuth } from "../Context";
-import Modal from "@mui/material/Modal";
-function ModalSignUp(){
-    const {openSignUp, setOpenSignUp } = useAuth();
-    const handleCloseSignUp = () => setOpenSignUp(false);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLasttName] = useState("");
-    const [mail, setMail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [correctCredential,setCorrectCredential]=useState(false);
-  const emailPattern = /^\S+@\S+\.\S+$/;
-    function mailInput(e) {
-        const mailSet = e.target.value;
-        setMail(mailSet);
-      }
-    
-      function passwordInput(e) {
-        const passwordSet = e.target.value;
-        setPassword(passwordSet);
-      }
-    
-      function firstNameInput(e) {
-        const firstNameSet = e.target.value;
-        setFirstName(firstNameSet);
-      }
-      function lastNameInput(e) {
-        const lastNameSet = e.target.value;
-        setLasttName(lastNameSet);
-      }
+// Importing necessary libraries and components
+import React, { useState } from "react";
+import Classes from "./Navbar.module.css"; // Importing CSS module for styling
+import { useAuth } from "../Context"; // Custom hook for authentication context
+import Modal from "@mui/material/Modal"; // Material-UI Modal component
 
-      async function handleBusSignup() {
-        if (!firstName) {
-          setCorrectCredential(false);
-          setErrorMessage("First name is required.");
-          return;
+function ModalSignUp() {
+  // State management using React hooks
+  
+  const { openSignUp, setOpenSignUp } = useAuth(); // Destructuring state and methods from authentication context(Manages the modal's open/close state)
+  const [firstName, setFirstName] = useState(""); // State for first name input field
+  const [lastName, setLastName] = useState(""); // State for last name input field
+  const [mail, setMail] = useState(""); // State for email input field
+  const [password, setPassword] = useState(""); // State for password input field
+  const [errorMessage, setErrorMessage] = useState(""); // State for error messages during signup
+  const [correctCredential, setCorrectCredential] = useState(false); // State to track correct credentials
+
+  // Regular expression pattern for validating email format
+  const emailPattern = /^\S+@\S+\.\S+$/;
+
+  // Handler for email input change and update state accordingly.
+  function mailInput(e) {
+    const mailSet = e.target.value;
+    setMail(mailSet);
+  }
+
+  // Handler for password input change
+  function passwordInput(e) {
+    const passwordSet = e.target.value;
+    setPassword(passwordSet);
+  }
+
+  // Handler for first name input change
+  function firstNameInput(e) {
+    const firstNameSet = e.target.value;
+    setFirstName(firstNameSet);
+  }
+
+  // Handler for last name input change
+  function lastNameInput(e) {
+    const lastNameSet = e.target.value;
+    setLasttName(lastNameSet);
+  }
+
+  // Function to handle signup process
+  async function handleBusSignup() {
+    // Validation checks before making the API call
+    if (!firstName) {
+      setCorrectCredential(false);
+      setErrorMessage("First name is required.");
+      return;
+    }
+    if (!mail || !password) {
+      setCorrectCredential(false);
+      setErrorMessage("Email and Password are required.");
+      return;
+    }
+    if (!emailPattern.test(mail)) {
+      setCorrectCredential(false);
+      setErrorMessage("Please enter a valid email address.");
+      return;
+    }
+
+    try {
+      // Making a POST request to the signup API endpoint
+      const response = await fetch(
+        "https://academics.newtonschool.co/api/v1/bookingportals/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            projectID: "wui79ffqiics",
+          },
+          body: JSON.stringify({
+            name: `${firstName} ${lastName}`,
+            email: mail,
+            password: password,
+            appType: "facebook", // Example app type
+          }),
         }
-        if (!mail || !password) {
-          setCorrectCredential(false);
-          setErrorMessage("Email and Password are required.");
-          return;
-        }
-        
-        if (!emailPattern.test(mail)) {
-          setCorrectCredential(false);
-          setErrorMessage("Please enter a valid email address.");
-          return;
-        }
-        try {
-          const response = await fetch(
-            "https://academics.newtonschool.co/api/v1/bookingportals/signup",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                projectID: "wui79ffqiics",
-              },
-              body: JSON.stringify({
-                name:`${firstName} ${lastName}`,
-                email: mail,
-                password: password,
-                appType: "facebook",
-              }),
-            }
-          );
-          if (response.status === 403) {
-            setCorrectCredential(false);
-            setErrorMessage("Email is already registered. Please go and log in instead.");
-          }
-            else if (response.ok) {
-            handleCloseSignUp();
-          } else {
-            const errorData = await response.json();
-            setErrorMessage(errorData.message);
-            setCorrectCredential(true);
-            handleCloseSignUp();
-          }
-        } catch (error) {
-          console.error("Error:", error);
-          setErrorMessage("An error occurred. Please try again.");
-        }
+      );
+
+      // Handling different response scenarios from the API
+      if (response.status === 403) {
+        // If email is already registered
+        setCorrectCredential(false);
+        setErrorMessage(
+          "Email is already registered. Please go and log in instead."
+        );
+      } else if (response.ok) {
+        // Successful signup
+        handleCloseSignUp();
+      } else {
+        // Other error scenarios
+        const errorData = await response.json();
+        setErrorMessage(errorData.message);
+        setCorrectCredential(true);
+        handleCloseSignUp();
       }
-    return(
-        <div>
-            <Modal
-        open={openSignUp}
-       
+    } catch (error) {
+      // Handling network or unexpected errors
+      console.error("Error:", error);
+      setErrorMessage("An error occurred. Please try again.");
+    }
+  }
+
+  // Handler to close the signup modal
+  const handleCloseSignUp = () => {
+    setOpenSignUp(false);
+  };
+
+  // JSX for rendering the signup modal
+  return (
+    <div>
+      <Modal
+        open={openSignUp} // Modal open state
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <div className={Classes.modalSignUpSection}>
-        <div className="w-[95%] flex flex-col gap-[10px]">
-        <div className="text-[22px] text-[#000] font-bold bg-[#fff] mt-[15px]">
+          <div className="w-[95%] flex flex-col gap-[10px]">
+            <div className="text-[22px] text-[#000] font-bold bg-[#fff] mt-[15px]">
               <p>SignUp or Create an account</p>
             </div>
-            <div className={Classes.closeBtnSignUp} onClick={handleCloseSignUp}></div>
-            <p className="errorDisplay" style={{display:correctCredential?"none":"",color:"red",textAlign:"center"}}>{errorMessage}</p>
+            <div
+              className={Classes.closeBtnSignUp}
+              onClick={handleCloseSignUp}
+            ></div>
+            {/* Displaying error message based on credential validity */}
+            <p
+              className="errorDisplay"
+              style={{
+                display: correctCredential ? "none" : "",
+                color: "red",
+                textAlign: "center",
+              }}
+            >
+              {errorMessage}
+            </p>
+            {/* Input fields for first name, last name, email, and password */}
             <div className="w-[100%] flex gap-[2%] mt-[10px]">
-            
-            <input
+              <input
                 type="text"
                 className="w-[49%] p-[10px] rounded-[5px] border-[0.5px] border-solid border-gray-200 focus:outline-none"
                 value={firstName}
@@ -114,15 +154,15 @@ function ModalSignUp(){
                 placeholder="Last name"
               />
             </div>
-            <div className="flex flex-col  gap-[10px]">
-            <input
+            <div className="flex flex-col gap-[10px]">
+              <input
                 type="email"
                 className="w-[100%] p-[10px] rounded-[5px] border-[0.5px] border-solid border-gray-200 focus:outline-none"
                 value={mail}
                 onChange={mailInput}
                 placeholder="Email address"
               />
-              
+
               <input
                 type="password"
                 className="w-[100%] p-[10px] rounded-[5px] border-[0.5px] border-solid border-gray-200 focus:outline-none"
@@ -131,17 +171,28 @@ function ModalSignUp(){
                 placeholder="New password"
               />
             </div>
+            {/* Continue button for signup */}
             <div className="w-[100%] flex items-center justify-center mt-[10px]">
-              <div className="text-[18px] text-[#fff] w-[100%] h-[43px] font-[600] rounded-[40px] bg-[#EF6614] cursor-pointer flex items-center justify-center" onClick={handleBusSignup}>Continue</div>
+              <div className="text-[18px] text-[#fff] w-[100%] h-[43px] font-[600] rounded-[40px] bg-[#EF6614] cursor-pointer flex items-center justify-center" onClick={handleBusSignup}>
+                Continue
+              </div>
             </div>
+            {/* Terms of use and privacy policy */}
             <div className="w-[100%] flex justify-center items-center mt-[10px] mb-[15px]">
-              <p className="w-[100%] text-[11px] text-[#8A8686] flex items-center">By logging in, I understand & agree to EaseMyTrip terms of use  and privacy policy</p>
+              <p className="w-[100%] text-[11px] text-[#8A8686] flex items-center">
+                By logging in, I understand & agree to EaseMyTrip terms of use
+                and privacy policy
+              </p>
             </div>
-        </div>
+          </div>
         </div>
       </Modal>
-
-        </div>
-    )
+    </div>
+  );
 }
+
+// Exporting ModalSignUp component as the default export from this module
 export default ModalSignUp;
+
+
+
