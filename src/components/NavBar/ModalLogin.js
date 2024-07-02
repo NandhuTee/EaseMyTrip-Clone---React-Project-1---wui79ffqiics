1c9da53aa1c7c6bbafb33a963f12ea72f677f126
Nandhini_T <rtnandhutee@gmail.com>
2024-07-03 04:33:50 +0530

    update

--- src/components/NavBar/ModalLogin.js
+++ src/components/NavBar/ModalLogin.js
@@ -1,50 +1,46 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useAuth } from "../Context";
 import Modal from "@mui/material/Modal";
-import Classes from "./Navbar.module.css"
+import Classes from "./Navbar.module.css";
 import ModalSignUp from "./ModalSignUp";
+
 function ModalLogin() {
-  const handleCloseLogin = () => setOpenLogin(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const { openLogin, setOpenLogin,openSignUp, setOpenSignUp, setIsLoggedIn } = useAuth();
+  const { openLogin, setOpenLogin, openSignUp, setOpenSignUp, setIsLoggedIn } = useAuth();
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
-  const [correctCredential,setCorrectCredential]=useState(false);
-  function mailInput(e) {
-    const mailSet = e.target.value;
-    setMail(mailSet);
-  }
+  const [errorMessage, setErrorMessage] = useState("");
+  const [correctCredential, setCorrectCredential] = useState(false);
+  const [userName, setUserName] = useState(""); // Initialize state for userName
 
-  function passwordInput(e) {
-    const passwordSet = e.target.value;
-    setPassword(passwordSet);
-  }
+  const handleCloseLogin = () => setOpenLogin(false);
 
-  async function handleLoginClick() {
-    
+  const handleLoginClick = async () => {
     try {
-      const response = await fetch(
-        "https://academics.newtonschool.co/api/v1/bookingportals/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            projectID: "uojmjpx76p25",
-          },
-          body: JSON.stringify({
-            email: mail,
-            password: password,
-            appType: "facebook",
-          }),
-        }
-      );
+      const response = await fetch("https://academics.newtonschool.co/api/v1/bookingportals/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          projectID: "uojmjpx76p25",
+        },
+        body: JSON.stringify({
+          email: mail,
+          password: password,
+          appType: "facebook",
+        }),
+      });
+
       if (response.ok) {
         const data = await response.json();
 
+        // Update localStorage
         localStorage.setItem("token", data.token);
         localStorage.setItem("userId", data.data._id);
-        localStorage.setItem("userName",data.data.name);
-        localStorage.setItem("photo","https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/590.jpg");
+        localStorage.setItem("userName", data.data.name);
+        localStorage.setItem("photo", "https://pixabay.com/illustrations/icon-user-male-avatar-business-5359553/");
+
+        // Update state
+        setUserName(data.data.name); // Update userName state
+
         setIsLoggedIn(true);
         handleCloseLogin();
       } else {
@@ -56,16 +52,16 @@ function ModalLogin() {
       console.error("Error:", error);
       setErrorMessage("An error occurred. Please try again.");
     }
-  }
+  };
 
-  const handleOpenSignUp= () => {
+  const handleOpenSignUp = () => {
     setOpenSignUp(true);
-}
+  };
+
   return (
     <div>
       <Modal
         open={openLogin}
-       
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
@@ -75,39 +71,47 @@ function ModalLogin() {
               <p>Login or Create an account</p>
             </div>
             <div className={Classes.closeBtn} onClick={handleCloseLogin}></div>
-            <div className="w-[100%] flex flex-col  mt-[10px]">
-              <p style={{display:!correctCredential?"none":"",color:"red",textAlign:"center"}}>{errorMessage}</p>
+            <div className="w-[100%] flex flex-col mt-[10px]">
+              <p style={{ display: !correctCredential ? "none" : "", color: "red", textAlign: "center" }}>{errorMessage}</p>
               <input
                 type="email"
                 className="p-[10px] border-[0.5px] border-solid border-gray-200 rounded-[5px] focus:outline-none mt-[10px]"
                 value={mail}
-                onChange={mailInput}
+                onChange={(e) => setMail(e.target.value)}
                 placeholder="Email address"
               />
-
               <input
                 type="password"
                 className="p-[10px] mt-[15px] border-[0.5px] border-solid border-gray-200 rounded-[5px] text-[15px] text-[#000000] bg-[#fff] w-[100%] focus:outline-none"
                 value={password}
-                onChange={passwordInput}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
               />
             </div>
             <div className="w-[100%] flex items-center justify-center mt-[10px]">
-              <div className="text-[18px] text-[#fff] w-[100%] h-[43px] font-[600] rounded-[40px] bg-[#EF6614] cursor-pointer flex items-center justify-center"  onClick={handleLoginClick}>Continue</div>
+              <div
+                className="text-[18px] text-[#fff] w-[100%] h-[43px] font-[600] rounded-[40px] bg-[#EF6614] cursor-pointer flex items-center justify-center"
+                onClick={handleLoginClick}
+              >
+                Continue
+              </div>
             </div>
             <div className="w-[100%] flex items-center justify-center">
-              <div className="text-[13px] text-[#0866FF]   rounded-[40px] cursor-pointer flex items-center justify-center" onClick={handleOpenSignUp}>Create New Account ?</div>
+              <div className="text-[13px] text-[#0866FF] rounded-[40px] cursor-pointer flex items-center justify-center" onClick={handleOpenSignUp}>
+                Create New Account ?
+              </div>
             </div>
             <div className="w-[100%] flex justify-center items-center mt-[10px] mb-[15px]">
-              <p className="w-[100%] text-[11px] text-[#8A8686] flex items-center">By logging in, I understand & agree to EaseMyTrip terms of use  and privacy policy</p>
+              <p className="w-[100%] text-[11px] text-[#8A8686] flex items-center">
+                By logging in, I understand & agree to EaseMyTrip terms of use and privacy policy
+              </p>
             </div>
           </div>
-          
         </div>
       </Modal>
-      {openSignUp &&(<ModalSignUp/>)}
+      {openSignUp && <ModalSignUp />}
     </div>
   );
 }
+
 export default ModalLogin;
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,78 +1,87 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Classes from "./Navbar.module.css";
 import Avatar from '@mui/material/Avatar';
 import { Box, Divider, Modal } from "@mui/material";
-import { Link,NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import ListItemButton from "@mui/material/ListItemButton";
 import { useAuth } from "../Context";
 import ModalLogin from "./ModalLogin";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
-    const UserIcon = localStorage.getItem("photo");
-    const UserName = localStorage.getItem("userName");
-    const isToken=localStorage.getItem("token");
-    const navigate = useNavigate();
-    const {
-      openLogin, setOpenLogin,isLoggedIn,setIsLoggedIn,
-    } = useAuth();
-    const [isDropdownhelpOpen, setDropdownhelpOpen] = useState(false);
-    const openDropdown = () => {
-      setDropdownOpen(true);
-    };
-  
-    const closeDropdown = () => {
-      setDropdownOpen(false);
-    };
-    const openDropdownhelp = () => {
-        setDropdownhelpOpen(true);
-      };
-    
-      const closeDropdownhelp = () => {
-        setDropdownhelpOpen(false);
-      };
-      const handleLoginLogout = () => {
-        if (isToken) {
-          localStorage.removeItem('token');
-          setIsLoggedIn(false);
-        }
-      };
-      const handleMyBooking=()=>{
-        navigate("/mybooking")
-      }
-      
-      const handleOpenLogin= () => setOpenLogin(true);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [userName, setUserName] = useState(localStorage.getItem("userName"));
+  const [userIcon, setUserIcon] = useState(localStorage.getItem("photo"));
+  const [isToken, setIsToken] = useState(localStorage.getItem("token"));
+  const navigate = useNavigate();
+  const { openLogin, setOpenLogin, isLoggedIn, setIsLoggedIn } = useAuth();
+  const [isDropdownhelpOpen, setDropdownhelpOpen] = useState(false);
+
+  useEffect(() => {
+    setUserName(localStorage.getItem("userName"));
+    setUserIcon(localStorage.getItem("photo"));
+    setIsToken(localStorage.getItem("token"));
+  }, [isLoggedIn]);
+
+  const openDropdown = () => {
+    setDropdownOpen(true);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
+  const openDropdownhelp = () => {
+    setDropdownhelpOpen(true);
+  };
+
+  const closeDropdownhelp = () => {
+    setDropdownhelpOpen(false);
+  };
+
+  const handleLoginLogout = () => {
+    if (isToken) {
+      localStorage.removeItem("token");
+      setIsLoggedIn(false);
+      setUserName(""); // Clear userName state on logout
+    }
+  };
+
+  const handleMyBooking = () => {
+    navigate("/mybooking");
+  };
+
+  const handleOpenLogin = () => setOpenLogin(true);
+
   return (
     <div className={Classes.navbarEase}>
       <div className={Classes.navClickSection}>
         <div className={Classes.navContent}>
           <div className={Classes.navLogoSection}>
             <NavLink to="/">
-            <img
-              className={Classes.logoEase}
-              src="https://www.easemytrip.com/images/brandlogo/emtlogo_new6.svg"
-              alt="logo Image"
-            />
+              <img
+                className={Classes.logoEase}
+                src="https://www.easemytrip.com/images/brandlogo/emtlogo_new6.svg"
+                alt="logo Image"
+              />
             </NavLink>
           </div>
           <div className={Classes.navRouteContent}>
             <div className={Classes.clickSection}>
               <Link className={Classes.linkSection} to={"/"}>
-              <h3 className={Classes.clickFLIGHTSH3}>FLIGHTS</h3>
+                <h3 className={Classes.clickFLIGHTSH3}>FLIGHTS</h3>
               </Link>
-              <Divider orientation="vertical"  style={{"height":"40%"}} />
+              <Divider orientation="vertical" style={{ height: "40%" }} />
               <Link className={Classes.linkSection} to={"/hotelhome"}>
-              <h3 className={Classes.clickHOTELSH3}>HOTELS</h3>
+                <h3 className={Classes.clickHOTELSH3}>HOTELS</h3>
               </Link>
-              <Divider orientation="vertical" style={{"height":"40%"}} />
+              <Divider orientation="vertical" style={{ height: "40%" }} />
               <Link className={Classes.linkSection} to={"/train"}>
-              <h3 className={Classes.clickTRAINSH3}>TRAINS</h3>
+                <h3 className={Classes.clickTRAINSH3}>TRAINS</h3>
               </Link>
-              <Divider orientation="vertical" style={{"height":"40%"}} />
+              <Divider orientation="vertical" style={{ height: "40%" }} />
               <Link className={Classes.linkSection} to={"/bus"}>
-              <h3 className={Classes.clickBUSH3}>BUS</h3>
+                <h3 className={Classes.clickBUSH3}>BUS</h3>
               </Link>
             </div>
           </div>
@@ -86,11 +95,11 @@ function Navbar() {
         </div>
       </div>
       <div className={Classes.navUser}>
-        <div className={Classes.myAcount}
-        onMouseEnter={openDropdown}
-        onMouseLeave={closeDropdown}
+        <div
+          className={Classes.myAcount}
+          onMouseEnter={openDropdown}
+          onMouseLeave={closeDropdown}
         >
-            
           <div className={Classes.userIconNav}>
             <img
               className={Classes.iconMan}
@@ -99,44 +108,48 @@ function Navbar() {
             />
           </div>
           <div className={Classes.navAcount}>
-          <p>{isToken? UserName:"My Account"}</p>
-                {isDropdownOpen && (
-                  <div className={Classes.dropdownContent}
-                  onMouseEnter={openDropdown}
-                    onMouseLeave={closeDropdown}
-                    >
-                    <div className={Classes.accountBox}>
-                      <div className={Classes.avatar}>
-                      {isToken? 
-                      <Avatar src={UserIcon}/>
-                    :<Avatar/>}
-                        </div>  
-                    <div className={Classes.loginBtnSection}>
-                    {isToken? UserName:<button className={Classes.btnLogin} onClick={handleOpenLogin}>LOGIN OR SIGNUP</button>}
-                    </div>
-                    
-                    <Divider className={Classes.dividerLogin}/>
-                    <div className={Classes.dropMyBookings}>
-                      <ListItemButton onClick={handleMyBooking}>
-                      <p className={Classes.bookingP}>My Booking</p>
-                      </ListItemButton>
-                    <ListItemButton onClick={handleLoginLogout}>
-                    <p className={Classes.bookingP} >Log Out</p>
-                    </ListItemButton>
-                    
-                    </div>
-                    </div> 
-                    
+            <p>{isToken ? userName : "My Account"}</p>
+            {isDropdownOpen && (
+              <div
+                className={Classes.dropdownContent}
+                onMouseEnter={openDropdown}
+                onMouseLeave={closeDropdown}
+              >
+                <div className={Classes.accountBox}>
+                  <div className={Classes.avatar}>
+                    {isToken ? (
+                      <Avatar src={userIcon} />
+                    ) : (
+                      <Avatar />
+                    )}
                   </div>
-                  
-                )}
-                {openLogin &&(<ModalLogin/>)}
+                  <div className={Classes.loginBtnSection}>
+                    {isToken ? (
+                      userName
+                    ) : (
+                      <button className={Classes.btnLogin} onClick={handleOpenLogin}>
+                        LOGIN OR SIGNUP
+                      </button>
+                    )}
+                  </div>
+                  <Divider className={Classes.dividerLogin} />
+                  <div className={Classes.dropMyBookings}>
+                    <ListItemButton onClick={handleMyBooking}>
+                      <p className={Classes.bookingP}>My Booking</p>
+                    </ListItemButton>
+                    <ListItemButton onClick={handleLoginLogout}>
+                      <p className={Classes.bookingP}>Log Out</p>
+                    </ListItemButton>
+                  </div>
+                </div>
+              </div>
+            )}
+            {openLogin && <ModalLogin />}
           </div>
-          
         </div>
       </div>
-      
     </div>
   );
 }
+
 export default Navbar;
